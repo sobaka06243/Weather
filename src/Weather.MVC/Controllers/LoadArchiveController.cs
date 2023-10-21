834c55d1c438faba438fbf886b7085391d20a039
Ivan <Ivan@KOMPUTER>
2023-10-21 10:39:59 +0700

    created form to loading files

---
+++ src/Weather.MVC/Controllers/LoadArchiveController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.MVC.Controllers
+{
+    public class LoadArchiveController : Controller
+    {
+        // GET: LoadArchiveController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: LoadArchiveController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
