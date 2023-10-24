81188a57d28163691033b26503c1948fdf574ecb
Ivan Kiselev <ivan.kiselev1999@outlook.com>
2023-10-24 11:55:31 +0700

    added summary to Parse()

--- src/Weather.Services/Services/IWeatherParser.cs
+++ src/Weather.Services/Services/IWeatherParser.cs
@@ -4,5 +4,13 @@ namespace Weather.Services.Services;
 
 public interface IWeatherParser
 {
+    /// <summary>
+    /// Parse xlsx file from given <paramref name="stream"/>
+    /// <para> 
+    /// <see cref="InvalidOperationException"/> if file is invalid.
+    /// </para>
+    /// </summary>
+    /// <param name="stream"></param>
+    /// <returns></returns>
     IEnumerable<WeatherData> Parse(Stream stream);
 }
